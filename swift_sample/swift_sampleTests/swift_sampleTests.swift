9d3f908aed1dabe33fe99539207cd4fca9cc9d8e
kentaroMiura <oikimutida@yahoo.co.jp>
2016-12-03 19:14:44 +0900

    project_add

---
+++ swift_sample/swift_sampleTests/swift_sampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  swift_sampleTests.swift
+//  swift_sampleTests
+//
+//  Created by Miura Kentaro on 2016/12/03.
+//  Copyright © 2016年 Miura Kentaro. All rights reserved.
+//
+
+import XCTest
+@testable import swift_sample
+
+class swift_sampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
