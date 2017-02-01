bb335650a50eb7e46f71f9680d1570fcd3045872
kentarokentaro <oikimutida@yahoo.co.jp>
2017-02-01 15:05:15 +0900

    Hello World

--- swift_sample/swift_sample/ViewController.swift
+++ swift_sample/swift_sample/ViewController.swift
@@ -12,12 +12,14 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Hello World
+        print("Hello World")
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
