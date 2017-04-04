8876b4d6298a70fd22aeedadb1900f7d52e6020a
kentaroMiura <oikimutida@yahoo.co.jp>
2017-04-04 20:30:32 +0900

    [add] Add Hello Class File in Project

---
+++ swift_sample/swift_sample/Model/Hello/HelloModel.swift
@@ -0,0 +1,13 @@
+//
+//  HelloModel.swift
+//  swift_sample
+//
+//  Created by Miura Kentaro on 2017/04/04.
+//  Copyright © 2017年 Miura Kentaro. All rights reserved.
+//
+
+import UIKit
+
+class HelloModel: NSObject {
+
+}
---
+++ swift_sample/swift_sample/View/Hello/HelloView.swift
@@ -0,0 +1,21 @@
+//
+//  HelloView.swift
+//  swift_sample
+//
+//  Created by Miura Kentaro on 2017/04/04.
+//  Copyright © 2017年 Miura Kentaro. All rights reserved.
+//
+
+import UIKit
+
+class HelloView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
